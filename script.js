9ca09dc4c8531824e69daee1e68beed013bd7f81
Hamza <hzamzami@protonmail.com>
2023-06-07 15:31:37 +0300

    fixed = counting as an op

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ const divide = function (num1, num2) {
     if (num2) {
         return num1 / num2;
     }
+    error = true;
     return "ERROR"
+    
 };
 const multiply = function (num1, num2) {
     return num1 * num2;
@@ -39,41 +41,43 @@ const isOperator = function (value) {
 
 const buttons = document.querySelectorAll('.row button');
 const display = document.getElementById('display')
-const operatorButtons = document.getElementsByClassName('operator');
+const operatorButtons = document.querySelectorAll('.operator');
 let opClicked = false;
 let opClearDisplay = false;
 let operandValue = 0;
 let operatorUsed = undefined;
 let error = false;
+let x = 13
 buttons.forEach((button) => button.addEventListener('click', () => {
     const buttonContent = button.innerText;
     let displayContent = display.innerText;
 
-    if (buttonContent === 'CLEAR' || displayContent === 'ERROR') {
+    if (buttonContent === 'CLEAR' || error) {
         opClicked = false;
         opClearDisplay = false;
         operandValue = 0;
         operatorUsed = undefined;
     }
-    if (displayContent === 'ERROR') {
-        error = true;
-    }
+
     if (buttonContent === 'CLEAR') {
         displayContent = 0
         error = false;
     }
 
-    if (isOperator(buttonContent)) {
+    if (isOperator(buttonContent) && !error) {
         if (opClicked && !opClearDisplay) {
             displayContent = (operate(operandValue, parseFloat(displayContent), operatorUsed))
         }
         else if (opClicked) {
             displayContent = (operate(operandValue, operandValue, operatorUsed))
         }
-        opClicked = true;
-        opClearDisplay = true;
-        operandValue = parseFloat(displayContent)
-        operatorUsed = buttonContent;
+        if (buttonContent !== '=') {
+            opClicked = true;
+            opClearDisplay = true;
+            operandValue = parseFloat(displayContent)
+            operatorUsed = buttonContent;
+        }
+
     }
     else if (displayContent === "0" || opClearDisplay || error) {
         if (isNumber(buttonContent)) {
@@ -93,9 +97,9 @@ buttons.forEach((button) => button.addEventListener('click', () => {
             displayContent.includes('.') ? displayContent : displayContent += buttonContent
         }
     }
-    // operatorButtons.forEach((btn) => {
-    //     btn.disabled = true;
-    // })
+    operatorButtons.forEach((btn) => {
+        btn.disabled = error;
+    })
     display.innerText = displayContent
 }))
 
