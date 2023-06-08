0b12339e3673c27d70bc4d150e37f7e5c05ca092
Hamza <hzamzami@protonmail.com>
2023-06-08 09:26:53 +0300

    made the delete button work and fixed further = issue

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const add = function (num1, num2) {
+    if (num1 + num2 === Infinity) {
+        error = true;
+    }
     return num1 + num2;
 };
 const subtract = function (num1, num2) {
@@ -10,9 +13,12 @@ const divide = function (num1, num2) {
     }
     error = true;
     return "ERROR"
-    
+
 };
 const multiply = function (num1, num2) {
+    if (num1 * num2 === Infinity) {
+        error = true;
+    }
     return num1 * num2;
 };
 
@@ -42,12 +48,12 @@ const isOperator = function (value) {
 const buttons = document.querySelectorAll('.row button');
 const display = document.getElementById('display')
 const operatorButtons = document.querySelectorAll('.operator');
+const deleteButton = document.getElementById('delete-btn')
 let opClicked = false;
 let opClearDisplay = false;
 let operandValue = 0;
 let operatorUsed = undefined;
 let error = false;
-let x = 13
 buttons.forEach((button) => button.addEventListener('click', () => {
     const buttonContent = button.innerText;
     let displayContent = display.innerText;
@@ -67,9 +73,11 @@ buttons.forEach((button) => button.addEventListener('click', () => {
     if (isOperator(buttonContent) && !error) {
         if (opClicked && !opClearDisplay) {
             displayContent = (operate(operandValue, parseFloat(displayContent), operatorUsed))
+            opClicked = false;
         }
-        else if (opClicked) {
+        else if (opClicked && buttonContent === '=') {
             displayContent = (operate(operandValue, operandValue, operatorUsed))
+            opClicked = false;
         }
         if (buttonContent !== '=') {
             opClicked = true;
@@ -82,12 +90,15 @@ buttons.forEach((button) => button.addEventListener('click', () => {
     else if (displayContent === "0" || opClearDisplay || error) {
         if (isNumber(buttonContent)) {
             displayContent = buttonContent;
+            opClearDisplay = false;
+            error = false;
         }
         else if (buttonContent == ".") {
             displayContent = "0."
+            opClearDisplay = false;
+            error = false;
         }
-        opClearDisplay = false;
-        error = false;
+
     }
     else {
         if (isNumber(buttonContent)) {
@@ -96,15 +107,16 @@ buttons.forEach((button) => button.addEventListener('click', () => {
         else if (buttonContent === ".") {
             displayContent.includes('.') ? displayContent : displayContent += buttonContent
         }
+        else if (buttonContent === 'DELETE') {
+            displayContent = displayContent.slice(0, -1);
+            if (displayContent === '') {
+                displayContent = 0;
+            }
+        }
     }
     operatorButtons.forEach((btn) => {
         btn.disabled = error;
     })
+    deleteButton.disabled = error;
     display.innerText = displayContent
 }))
-
-let num1 = 5;
-let num2 = 10;
-let operator = "/";
-
-console.log(operate(num1, num2, operator))
