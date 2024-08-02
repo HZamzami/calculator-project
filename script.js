1e82385d66d9f0ff03e6492743a1b41bdae0d3b9
HZamzami <107877308+HZamzami@users.noreply.github.com>
2024-08-02 09:33:01 +0300

    new edition

--- script.js
+++ script.js
@@ -1,122 +1,134 @@
-const add = function (num1, num2) {
-    if (num1 + num2 === Infinity) {
-        error = true;
-    }
-    return num1 + num2;
-};
-const subtract = function (num1, num2) {
-    return num1 - num2;
-};
-const divide = function (num1, num2) {
-    if (num2) {
-        return num1 / num2;
-    }
-    error = true;
-    return "ERROR"
-
-};
-const multiply = function (num1, num2) {
-    if (num1 * num2 === Infinity) {
-        error = true;
-    }
-    return num1 * num2;
-};
-
-const operate = function (num1, num2, operator) {
-    switch (operator) {
-        case "+":
-            return add(num1, num2);
-        case "-":
-            return subtract(num1, num2);
-        case "*":
-            return multiply(num1, num2);
-        case "/":
-            return divide(num1, num2)
-        default:
-            return "WHOT"
-    }
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function operate(operator, a, b) {
+    return operator(a, b);
 }
 
-const isNumber = function (value) {
-    return /\d/.test(value)
-}
 
-const isOperator = function (value) {
-    return /[-*+\/=]/.test(value)
-}
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const deleteButton = document.querySelector('[data-delete]');
+const mainDisplay = document.querySelector('[data-display-main]');
+const opDisplay = document.querySelector('[data-display-op]');
+const clearButton = document.querySelector('[data-clear]');
+const equalsButton = document.querySelector('[data-equals]');
 
-const buttons = document.querySelectorAll('.row button');
-const display = document.getElementById('display')
-const operatorButtons = document.querySelectorAll('.operator');
-const deleteButton = document.getElementById('delete-btn')
-let opClicked = false;
-let opClearDisplay = false;
-let operandValue = 0;
-let operatorUsed = undefined;
-let error = false;
-buttons.forEach((button) => button.addEventListener('click', () => {
-    const buttonContent = button.innerText;
-    let displayContent = display.innerText;
 
-    if (buttonContent === 'CLEAR' || error) {
-        opClicked = false;
-        opClearDisplay = false;
-        operandValue = 0;
-        operatorUsed = undefined;
+class Calculator {
+    constructor(opDisplayElement, mainDisplayElement) {
+        this.opDisplay = opDisplayElement;
+        this.mainDisplay = mainDisplayElement;
+        this.opDisplayText = opDisplayElement.textContent;
+        this.mainDisplayText = mainDisplay.textContent;
     }
 
-    if (buttonContent === 'CLEAR') {
-        displayContent = 0
-        error = false;
+    clear() {
+        this.mainDisplayText = '0';
+        this.opDisplayText = '';
+        this.chosenOperation = null;
+        this.operatorClicked = false;
+        this.updateDisplay()
     }
 
-    if (isOperator(buttonContent) && !error) {
-        if (opClicked && !opClearDisplay) {
-            displayContent = (operate(operandValue, parseFloat(displayContent), operatorUsed))
-            opClicked = false;
-        }
-        else if (opClicked && buttonContent === '=') {
-            displayContent = (operate(operandValue, operandValue, operatorUsed))
-            opClicked = false;
-        }
-        if (buttonContent !== '=') {
-            opClicked = true;
-            opClearDisplay = true;
-            operandValue = parseFloat(displayContent)
-            operatorUsed = buttonContent;
-        }
 
+    delete() {
+        this.mainDisplayText = this.mainDisplayText.slice(0, -1);
+        if (this.mainDisplayText == "") {
+            this.mainDisplayText = "0"
+        }
+        this.updateDisplay()
     }
-    else if (displayContent === "0" || opClearDisplay || error) {
-        if (isNumber(buttonContent)) {
-            displayContent = buttonContent;
-            opClearDisplay = false;
-            error = false;
-        }
-        else if (buttonContent == ".") {
-            displayContent = "0."
-            opClearDisplay = false;
-            error = false;
-        }
 
-    }
-    else {
-        if (isNumber(buttonContent)) {
-            displayContent += buttonContent;
+    appendNumber(number) {
+        if (this.mainDisplayText.includes(".") && number == ".") {
+            return;
         }
-        else if (buttonContent === ".") {
-            displayContent.includes('.') ? displayContent : displayContent += buttonContent
-        }
-        else if (buttonContent === 'DELETE') {
-            displayContent = displayContent.slice(0, -1);
-            if (displayContent === '') {
-                displayContent = 0;
+        if (this.mainDisplayText == '0' || this.operatorClicked) {
+            this.mainDisplayText = number;
+            if (number == ".") {
+                this.mainDisplayText = "0."
             }
         }
+        else {
+            this.mainDisplayText = `${this.mainDisplayText}${number}`
+        }
+        this.operatorClicked = false;
+        this.updateDisplay();
     }
-    operatorButtons.forEach((btn) => {
-        btn.disabled = error;
+
+    chooseOperation(opText) {
+        if (this.chosenOperation != null) {
+            this.compute();
+        }
+        this.opDisplayText = `${this.mainDisplayText} ${opText}`
+        this.chosenOperation = opText;
+        this.operatorClicked = true;
+        this.updateDisplay();
+    }
+
+    compute() {
+        if (this.chosenOperation != null) {
+            const firstOperand = Number(this.opDisplayText.slice(0, -1));
+            const secondOperand = Number(this.mainDisplayText);
+            const result = operate(operatorMap[this.chosenOperation], firstOperand, secondOperand)
+            this.opDisplayText = `${this.opDisplayText} ${secondOperand} =`;
+            this.mainDisplayText = `${result}`;
+            this.operatorClicked = true;
+            this.chosenOperation = null;
+        }
+        this.updateDisplay();
+    }
+
+    updateDisplay() {
+        this.opDisplay.textContent = this.opDisplayText;
+        this.mainDisplay.textContent = this.mainDisplayText;
+    }
+
+    
+}
+
+const operatorMap = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+const calculator = new Calculator(opDisplay, mainDisplay);
+
+numberButtons.forEach((numButton) => {
+    numButton.addEventListener('click', () => {
+
+        numText = numButton.textContent;
+        calculator.appendNumber(numText);
     })
-    deleteButton.disabled = error;
-    display.innerText = displayContent
-}))
+})
+
+operationButtons.forEach((opButton) => {
+    opButton.addEventListener('click', () => {
+        opText = opButton.textContent;
+        calculator.chooseOperation(opText);
+    })
+})
+
+clearButton.addEventListener('click', () => {
+    calculator.clear()
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.compute();
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete()
+})
